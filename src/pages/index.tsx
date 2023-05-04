3e035b435f88cdf8252b455dd3f604a5665191ce
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-05-04 22:00:56 +0300

    Add sign in and sign out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { signIn } from "next-auth/react"
+import { signIn, signOut, useSession  } from "next-auth/react"
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data: session, status } = useSession();
 
   return (
-    <>
+    <div>
       <Head>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <button onClick={() => {void signIn("instagram"); }}>
-          <div>Sign in</div>
-        </button>
+        <div>
+          {status !== "authenticated" && 
+          <button onClick={() => {void signIn("instagram"); }}>
+            <div>Sign in</div>
+          </button>}
+          {status === "authenticated" && 
+          <button onClick={() => {void signOut(); }}>
+            <div>Sign out</div>
+          </button>}
+        </div>
       </main>
-    </>
+    </div>
   );
 };
 
