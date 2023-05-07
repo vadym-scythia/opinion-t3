58d712bdc60c2846010683c69082a3b92cf37b85
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-05-07 21:14:53 +0300

    Fix Application error: a client-side exception has occurred

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react"
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
@@ -11,26 +9,24 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <Head>
-        <div className="flex flex-col items-center justify-center bg-slate-500">
-          {
-            isNotAuth &&
-            <button onClick={() => { void signIn("instagram"); }}>
-              <div>Sign in</div>
-            </button>
-          }
-          {
-            isAuth &&
-            <button onClick={() => { void signOut(); }}>
-              <div>Sign out</div>
-            </button> &&
-            <Link href="/user-profile">
-              <a>My profile</a>
-            </Link> &&
-            <div>WIP: here will be search wizard</div>
-          }
-        </div>
-      </Head>
+      <div className="flex flex-col items-center justify-center bg-slate-500">
+        {
+          status !== "authenticated" &&
+          <button onClick={() => { void signIn("instagram"); }}>
+            <div>Sign in</div>
+          </button>
+        }
+        {
+          isAuth &&
+          <button onClick={() => { void signOut(); }}>
+            <div>Sign out</div>
+          </button> &&
+          <Link href="/user-profile">
+            <a>My profile</a>
+          </Link> &&
+          <div>WIP: here will be search wizard</div>
+        }
+      </div>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {
           isNotAuth &&
