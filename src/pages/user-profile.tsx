8ee816bb4293040b1ba7488060096ef8b52d546a
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-05-08 00:17:07 +0300

    Comment the user image
    Not obvious how to get the picture from Instagram

--- src/pages/user-profile.tsx
+++ src/pages/user-profile.tsx
@@ -13,14 +13,19 @@ const UserProfile: NextPage = () => {
         return <div>User data is not available</div>;
     }
 
-    if (!session.user.image) {
-        return <div>User image is not available</div>
+    if (!session.user.name) {
+        return <div>User name is not available</div>;
     }
+
+    //todo: need to figure out, how to get the picture
+    // if (!session.user.image) {
+    //     return <div>User image is not available</div>
+    // }
 
     return (
         <div>
             <h1>User Profile</h1>
-            <Image src={session.user.image} alt="Profile picture" />
+            {/* <Image src={session.user.image} alt="Profile picture" /> */}
             <p>Username: {session.user.name}</p>
         </div>
     );
